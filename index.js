4f8d4521360730e35595474612cf6716250a67f9
SuperChump123 <greatdog14@gmail.com>
2023-10-31 18:32:42 -0400

    commit

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.get('/api/messages', (req, res) => {
 
 // start server on port 3000
 app.listen(3000, () => {
-  console.log('server is listening on port 3000');
+  console.log('server is listening on port 3000!');
 });
